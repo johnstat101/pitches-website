710f3ca1d9d1364175ee8acf6b9ed54bfe051c12
Rea <rmrea@kenya-airways.com>
2022-02-07 14:31:36 +0300

    update index.html

--- app/main/views.py
+++ app/main/views.py
@@ -13,7 +13,7 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    # Getting movies by category
+    return render_template("index.html")
 
 @main.route('/user/<uname>')
 def profile(uname):
