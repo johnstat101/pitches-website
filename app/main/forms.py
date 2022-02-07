cbdc7677f9f50296bef68b799e8a14db05741fc5
Rea <rmrea@kenya-airways.com>
2022-02-07 12:00:03 +0300

    add errors and forms file

---
+++ app/main/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import InputRequired
+
+class ReviewForm(FlaskForm):
+    title = StringField('Review title',validators=[InputRequired()])
+    review = TextAreaField('Movie review')
+    submit = SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [InputRequired()])
+    submit = SubmitField('Submit')
