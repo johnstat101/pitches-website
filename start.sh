9095bf4a6ec32cc1769d7eb57123bb1ab150c59b
Rea <rmrea@kenya-airways.com>
2022-02-08 06:55:29 +0300

    edit environ variables

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 export SECRET_KEY='<Flask WTF Secret Key>'
 export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://postgres:Njoro@localhost/watchlist'
+export DATABASE_URL='postgresql+psycopg2://postgres:Njoro@localhost/watchlist'
 export MAIL_USERNAME='john.kimani@student.moringaschool.com'
 export MAIL_PASSWORD='Moringa101'
 
