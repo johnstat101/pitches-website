51e43c131cc46a54d129184c268a766b4b8f3f5b
Rea <rmrea@kenya-airways.com>
2022-02-08 16:10:08 +0300

    edit confis

--- config.py
+++ config.py
@@ -32,10 +32,10 @@ class ProdConfig(Config):
 
 
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Njoro@localhost/watchlist_test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Njoro@localhost/watchlist_test'
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Njoro@localhost/watchlist'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Njoro@localhost/watchlist'
     DEBUG = True
 
 config_options = {
